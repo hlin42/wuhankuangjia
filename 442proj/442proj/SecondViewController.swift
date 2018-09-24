63beae7d8feda1092f77374244e5acfd4c7ca206
jchen247 <43354842+jchen247@users.noreply.github.com>
2018-09-23 20:27:25 -0400

    Add files via upload
    made second view controller for separate views. also made draft of data structure we are going to use later. and simple mission prompt

---
+++ 442proj/442proj/SecondViewController.swift
@@ -0,0 +1,34 @@
+//
+//  SecondViewController.swift
+//  442proj
+//
+//  Created by Jie Chen on 9/22/18.
+//  Copyright © 2018 Jie Chen. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    @IBOutlet weak var Testing: UILabel!
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        
+        self.Testing.text = Testing_String
+        add_Mission()
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- 442proj/442proj/ViewController.swift
+++ 442proj/442proj/ViewController.swift
@@ -8,52 +8,138 @@
 
 import UIKit
 
-class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+var Testing_String = "YOYOYOOYOY"
+class Mission_Storage {
+    var Latitude : Float = 0.0
+    var Longitude : Float = 0.0
+    var has_Next : Bool = false
+    var Next : Mission_Storage? = nil
+}
+var Overall_Data_Storage = [Int : Mission_Storage]()
 
+func add_Mission (key: Int, Latitude: Float, Longitude:Float){
+    // 2 case, if key already exist, add next_step of the mission
+    //if not, initilize first step of the mission
+    if Overall_Data_Storage[key] != nil{
+         var latest_Mission = Overall_Data_Storage[key]
+         let temp = Mission_Storage()
+         temp.Latitude = Latitude
+         temp.Longitude = Longitude
+        while(latest_Mission!.has_Next ){ //last_Mission will the currently last step
+            latest_Mission?.has_Next = true // changing the value to true has no impact on pervious steps
+                                            // but since we are making currenly last step into second to last
+                                            // it is necessary
+            latest_Mission = latest_Mission!.Next
+        }
+        // When we exit the loop latest_Mission.has_Next is false, that means last_Mission.Next is null
+        // Following line is to assign our pre declare temp Mission_Storage class to last_Mission.Next
+        latest_Mission?.Next = temp
+    }
+    else{
+        let temp = Mission_Storage()
+        temp.Latitude = Latitude
+        temp.Longitude = Longitude
+        Overall_Data_Storage[key] = temp
+        
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigationControllerDelegate {
     @IBOutlet weak var Main: UIButton!
     @IBOutlet weak var Camera: UIButton!
-    
     @IBOutlet weak var userImage: UIImageView!
+    
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let testing = Mission_Storage()
+        testing.Latitude = 111.11
+        testing.Longitude = 100.23
+        Overall_Data_Storage[0] = testing
+        Testing_String = (Overall_Data_Storage[0]?.Latitude.description)! + " and " + testing.Longitude.description
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     @IBAction func CameraAction(_ sender: UIButton) {
         
         if UIImagePickerController.isSourceTypeAvailable(.camera){
             let imagepicker = UIImagePickerController()
                 imagepicker.delegate = self
                 imagepicker.allowsEditing = false
-                imagepicker.sourceType = UIImagePickerControllerSourceType.camera
+                imagepicker.sourceType = UIImagePickerController.SourceType.camera
                 self.present(imagepicker,animated: false,completion: nil)
         }
         else{
             let imagepicker = UIImagePickerController()
             imagepicker.delegate = self
             imagepicker.allowsEditing = false
-            imagepicker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+            imagepicker.sourceType = UIImagePickerController.SourceType.photoLibrary
             self.present(imagepicker,animated: false,completion: nil)
             
         }
         
     }
+    
+    
+    
+    
+    
+    
     @IBAction func MainAction(_ sender: UIButton) {
-        
+
         let picker = UIImagePickerController()
         picker.delegate = self
         picker.sourceType = .photoLibrary
         present(picker, animated:true, completion:nil)
         
     }
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-        if let image=info[UIImagePickerControllerOriginalImage] as? UIImage {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+// Local variable inserted by Swift 4.2 migrator.
+let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+
+        if let image=info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage {
             userImage.image = image
         }
         else{
@@ -66,3 +152,13 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate,UINavigat
     
 }
 
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+	return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+	return input.rawValue
+}
