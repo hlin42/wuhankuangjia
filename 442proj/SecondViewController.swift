cec77f06ca2a3344f99ed9edb9e02b7c7b28485c
kcheung8 <kcheung8@buffalo.edu>
2018-12-03 18:05:03 -0500

    deleted old files with newer files

--- 442proj/SecondViewController.swift
+++ 442proj/SecondViewController.swift
@@ -14,12 +14,12 @@ class SecondViewController: UIViewController {
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        for (k, v) in Overall_Data_Storage{
-            if k == 0{
-                continue
-            }
-            Testing_String +=  "\n\nMission Code:  \(k) \n Description: \(v.description)\n            "
-        Testing.text = Testing_String
+        //for (k, v) in Overall_Data_Storage{
+        //    if k == 0{
+          //      continue
+           // }
+            //Testing_String +=  "\n\nMission Code:  \(k) \n Description: \(v.description)\n            "
+        Testing.text = "Mission Code: 001 \n Description: Basic UB Exploration \n Number of Steps: 2 \n -------------------\nMission Code: 002 \n Description: Advance UB Exploration \n Number of Steps: 5 \n -------------------\nMission Code: 003 \n Description: Tour of Life of Computer Engineers \n Number of Steps: 2 \n -------------------\n "
             
         }
         // Do any additional setup after loading the view.
@@ -35,4 +35,4 @@ class SecondViewController: UIViewController {
     }
     */
 
-}
+//}
